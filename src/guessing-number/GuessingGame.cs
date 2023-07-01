28432e76cc9b9ae6329941fd9e047dd0cd766b1a
Carlos Vieira <carlepsvieira@gmail.com>
2023-07-01 11:37:24 -0300

    req 02

--- src/guessing-number/GuessingGame.cs
+++ src/guessing-number/GuessingGame.cs
@@ -4,62 +4,74 @@ namespace guessing_number;
 
 public class GuessNumber
 {
-    //In this way we are passing the random number generator by dependency injection
-    private IRandomGenerator random;
-    public GuessNumber() : this(new DefaultRandom()){}
-    public GuessNumber(IRandomGenerator obj)
-    {
-        this.random = obj;
+  //In this way we are passing the random number generator by dependency injection
+  private IRandomGenerator random;
+  public GuessNumber() : this(new DefaultRandom()) { }
+  public GuessNumber(IRandomGenerator obj)
+  {
+    this.random = obj;
 
-        userValue = 0;
-        randomValue = 0;
+    userValue = 0;
+    randomValue = 0;
+  }
+
+  //user variables
+  public int userValue;
+  public int randomValue;
+
+  public int maxAttempts;
+  public int currentAttempts;
+
+  public int difficultyLevel;
+
+  public bool gameOver;
+
+  //1 - Imprima uma mensagem de saudação
+  public string Greet()
+  {
+    return "---Bem-vindo ao Guessing Game--- /n Para começar, tente adivinhar o número que eu pensei, entre -100 e 100!";
+  }
+
+  //2 - Receba a entrada da pessoa usuária e converta para Int
+  //5 - Adicione um limite de tentativas.
+  public string ChooseNumber(string userEntry)
+  {
+    if (!int.TryParse(userEntry, out int number))
+    {
+      return "Entrada inválida! Não é um número.";
     }
 
-    //user variables
-    public int userValue;
-    public int randomValue;
-
-    public int maxAttempts;
-    public int currentAttempts;
-
-    public int difficultyLevel;
-
-    public bool gameOver;
-
-    //1 - Imprima uma mensagem de saudação
-    public string Greet()
+    if (number < -100 || number > 100)
     {
-        return "---Bem-vindo ao Guessing Game--- /n Para começar, tente adivinhar o número que eu pensei, entre -100 e 100!";
+      userValue = 0;
+      return "Entrada inválida! Valor não está no range.";
     }
 
-    //2 - Receba a entrada da pessoa usuária e converta para Int
-    //5 - Adicione um limite de tentativas.
-    public string ChooseNumber(string userEntry)
-    {
-        throw new NotImplementedException();
-    }
+    userValue = number;
+    return "Número escolhido!";
+  }
 
-    //3 - Gere um número aleatório
-    public string RandomNumber()
-    {
-        throw new NotImplementedException();
-    }
+  //3 - Gere um número aleatório
+  public string RandomNumber()
+  {
+    throw new NotImplementedException();
+  }
 
-    //6 - Adicione níveis de dificuldade
-    public string RandomNumberWithDifficult()
-    {
-        throw new NotImplementedException();
-    }
+  //6 - Adicione níveis de dificuldade
+  public string RandomNumberWithDifficult()
+  {
+    throw new NotImplementedException();
+  }
 
-    //4 - Verifique a resposta da jogada
-    public string AnalyzePlay()
-    {
-        throw new NotImplementedException();
-    }
+  //4 - Verifique a resposta da jogada
+  public string AnalyzePlay()
+  {
+    throw new NotImplementedException();
+  }
 
-    //7 - Adicione uma opção para reiniciar o jogo
-    public void RestartGame()
-    {
-        throw new NotImplementedException();
-    }
+  //7 - Adicione uma opção para reiniciar o jogo
+  public void RestartGame()
+  {
+    throw new NotImplementedException();
+  }
 }
